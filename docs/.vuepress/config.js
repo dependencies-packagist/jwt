7acd25cb2d60f9ce3634cdb28bc2b4d819ec39ba
jundayw <jundayw@126.com>
2025-04-15 21:34:39 +0800

    CI

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import {defineUserConfig} from 'vuepress'
 import {viteBundler} from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+    base: "/jwt/",
     theme: defaultTheme({
         logo: '/images/logo.webp',
         locales: {
